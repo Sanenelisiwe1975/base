54a95678d73d860fd2fa80692f1d467b3555f719
sasimatjhb024 <sasimatjhb024@student.wethinkcode.co.za>
2025-10-14 13:03:18 +0200

    updated

--- Baxela/middleware.ts
+++ Baxela/middleware.ts
@@ -1,14 +1,21 @@
 import createMiddleware from 'next-intl/middleware';
-
+ 
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'es', 'fr', 'de', 'ja', 'zh', 'af', 'nr', 'nso', 'st', 'ss', 'ts', 'tn', 've', 'xh', 'zu'],
-
+ 
   // Used when no locale matches
   defaultLocale: 'en'
 });
-
+ 
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: [
+    // Match all pathnames except for
+    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … the ones containing a dot (e.g. `favicon.ico`)
+    '/((?!api|_next|_vercel|.*\\..*).*)',
+    // However, match all pathnames within `/` (e.g. `/en`, `/es`, `/de`, etc.)
+    '/(en|es|fr|de|ja|zh|af|nr|nso|st|ss|ts|tn|ve|xh|zu)/:path*'
+  ]
 };
